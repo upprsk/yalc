e8344c41d9a7f02470d9c6e585fe928f83c29fc0
Lucas Ross <lucassilveiraross@gmail.com>
2025-05-26 14:26:03 -0300

    test: :test_tube: added test for slicing an array

--- tests/test_ir.cpp
+++ tests/test_ir.cpp
@@ -809,6 +809,30 @@ func c_printf(fmt: [*]const u8, ...);
 )");
 
     ctx.tags.pop_back();
+    ctx.tags.emplace_back("slices");
+
+    run_test(ctx, p, "count spaces in string",
+             R"(
+module main;
+
+func main() i32 {
+    var arr = [_]i32{ 1, 2, 3, 4 };
+    var s = arr[:];
+
+    var i: usize = 0;
+    while i < s.len {
+        defer i = i + 1;
+        c_printf("s[%d] = %d\n".ptr, i, s[i]);
+    }
+
+    return 0;
+}
+
+@extern(link_name="printf")
+func c_printf(fmt: [*]const u8, ...);
+)");
+
+    ctx.tags.pop_back();
 
     print_test_results("IR", ctx);
     return {ctx.ok, ctx.failed, ctx.skipped};
