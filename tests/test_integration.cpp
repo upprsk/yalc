ac35613d847e5a059ef4a55a338f7c6c366e2dda
Lucas Ross <lucassilveiraross@gmail.com>
2025-06-18 12:02:19 -0300

    test: :test_tube: updated tests and added parser tests

--- tests/test_integration.cpp
+++ tests/test_integration.cpp
@@ -6,11 +6,15 @@
 #include <libassert/assert.hpp>
 #include <nlohmann/json.hpp>
 
+#include "ast.hpp"
 #include "error_reporter.hpp"
 #include "file_store.hpp"
+#include "parser.hpp"
 #include "tests.hpp"
 #include "tokenizer.hpp"
 #include "utils.hpp"
+
+namespace rv = std::ranges::views;
 
 namespace yal::tests {
 // NOLINTBEGIN(readability-function-cognitive-complexity)
@@ -72,10 +76,119 @@ yay!)"},
     return tb;
 }
 
+auto parser() -> ut::Test {
+    auto tb = ut::group("parser");
+
+    std::vector<std::pair<std::string, std::string>> tests{
+        {           "empty file",                ""},
+
+        {          "module decl",    "module main;"},
+        {"invalid module decl 1",             "mod"},
+        {"invalid module decl 2",          "module"},
+        {"invalid module decl 3",      "module 134"},
+        {"invalid module decl 4",     "module 134;"},
+        {"invalid module decl 5",     "modul main;"},
+        {"invalid module decl 6",  "module 'main';"},
+
+        {            "globals 1", R"~~(module main;
+var a;
+var b: usize;
+var c = 42;
+var d: usize = 42;
+
+var x, y, z = 0, 1, 2;
+var i, j: usize;
+var k, l: usize, i32 = 12, 3;
+)~~"},
+        {    "invalid globals 1", R"~~(module main;
+
+var c
+
+var a,: i32,  10
+var b: = 
+
+var global: usize;)~~"},
+        {    "invalid globals 2", R"~~(module main;
+
+var 
+)~~"},
+        {    "invalid globals 3", R"~~(module main;
+
+var global
+)~~"},
+        {    "invalid globals 4", R"~~(module main;
+
+var global:
+)~~"},
+        {    "invalid globals 5", R"~~(module main;
+
+var global: i32
+)~~"},
+        {    "invalid globals 6", R"~~(module main;
+var
+var good: i32;
+)~~"},
+        {    "invalid globals 7", R"~~(module main;
+var global
+var good: i32;
+)~~"},
+        {    "invalid globals 8", R"~~(module main;
+var global: usize
+var good: i32;
+)~~"},
+        {    "invalid globals 9", R"~~(module main;
+var global: usize =
+var good: i32;
+)~~"},
+        {   "invalid globals 10", R"~~(module main;
+var global: usize = 10
+var good: i32;
+)~~"},
+        {   "invalid globals 11", R"~~(module main;
+var global =
+var good: i32;
+)~~"},
+        {   "invalid globals 12", R"~~(module main;
+var global = 10
+var good: i32;
+)~~"},
+        {            "globals 2", R"~~(module main;
+var global: usize = 10;
+var good: i32;
+)~~"},
+        {            "globals 3", R"~~(module main;
+var global = 10;
+var good: i32;
+)~~"},
+    };
+
+    for (auto&& [name, input] : tests) {
+        ut::add(tb, name,
+                [name = name, input = std::move(input)](
+                    ut::Context const& ctx) -> nlohmann::json {
+                    auto fs = yal::FileStore{};
+                    auto er = yal::ErrorReporter{&fs, ctx.out};
+
+                    auto fileid = fs.add_file_and_contents(
+                        fmt::format(":memory:{}", name), input);
+                    auto tokens = tokenize(er.for_file(fileid));
+
+                    auto       ast = ast::Ast{};
+                    ast::Node* root =
+                        parse_into_ast(tokens, ast, er.for_file(fileid));
+
+                    return *root;
+                });
+    }
+
+    return tb;
+}
+
 auto integration() -> ut::Test {
     auto tb = ut::group("integration");
 
     ut::add(tb, tokenizer());
+    ut::add(tb, parser());
 
     return tb;
 }
