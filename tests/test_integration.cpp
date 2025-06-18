3e84abbdfbfc7947311e62b07e9f80e30334d434
Lucas Ross <lucassilveiraross@gmail.com>
2025-06-18 16:01:55 -0300

    tests(parser): :test_tube: added tests for attributes

--- tests/test_integration.cpp
+++ tests/test_integration.cpp
@@ -57,6 +57,10 @@ auto tokenizer() -> ut::Test {
 
         {             "shbang",                                   R"(#!/usr/local/bin/yalc
 yay!)"},
+
+        {"unbalanced parens 1",                                                    "(({))"},
+        {"unbalanced parens 2",                                                  "(({[}))"},
+        {"unbalanced parens 3",                                                  "([{[}))"},
     };
 
     for (auto&& [name, input] : tests) {
@@ -80,17 +84,17 @@ auto parser() -> ut::Test {
     auto tb = ut::group("parser");
 
     std::vector<std::pair<std::string, std::string>> tests{
-        {           "empty file",                ""},
+        {                       "empty file",                ""},
 
-        {          "module decl",    "module main;"},
-        {"invalid module decl 1",             "mod"},
-        {"invalid module decl 2",          "module"},
-        {"invalid module decl 3",      "module 134"},
-        {"invalid module decl 4",     "module 134;"},
-        {"invalid module decl 5",     "modul main;"},
-        {"invalid module decl 6",  "module 'main';"},
+        {                      "module decl",    "module main;"},
+        {            "invalid module decl 1",             "mod"},
+        {            "invalid module decl 2",          "module"},
+        {            "invalid module decl 3",      "module 134"},
+        {            "invalid module decl 4",     "module 134;"},
+        {            "invalid module decl 5",     "modul main;"},
+        {            "invalid module decl 6",  "module 'main';"},
 
-        {            "globals 1", R"~~(module main;
+        {                        "globals 1", R"~~(module main;
 var a;
 var b: usize;
 var c = 42;
@@ -100,7 +104,7 @@ var x, y, z = 0, 1, 2;
 var i, j: usize;
 var k, l: usize, i32 = 12, 3;
 )~~"},
-        {    "invalid globals 1", R"~~(module main;
+        {                "invalid globals 1", R"~~(module main;
 
 var c
 
@@ -108,78 +112,161 @@ var a,: i32,  10
 var b: = 
 
 var global: usize;)~~"},
-        {    "invalid globals 2", R"~~(module main;
+        {                "invalid globals 2", R"~~(module main;
 
 var 
 )~~"},
-        {    "invalid globals 3", R"~~(module main;
+        {                "invalid globals 3", R"~~(module main;
 
 var global
 )~~"},
-        {    "invalid globals 4", R"~~(module main;
+        {                "invalid globals 4", R"~~(module main;
 
 var global:
 )~~"},
-        {    "invalid globals 5", R"~~(module main;
+        {                "invalid globals 5", R"~~(module main;
 
 var global: i32
 )~~"},
-        {    "invalid globals 6", R"~~(module main;
+        {                "invalid globals 6", R"~~(module main;
 var
 var good: i32;
 )~~"},
-        {    "invalid globals 7", R"~~(module main;
+        {                "invalid globals 7", R"~~(module main;
 var global
 var good: i32;
 )~~"},
-        {    "invalid globals 8", R"~~(module main;
+        {                "invalid globals 8", R"~~(module main;
 var global: usize
 var good: i32;
 )~~"},
-        {    "invalid globals 9", R"~~(module main;
+        {                "invalid globals 9", R"~~(module main;
 var global: usize =
 var good: i32;
 )~~"},
-        {   "invalid globals 10", R"~~(module main;
+        {               "invalid globals 10", R"~~(module main;
 var global: usize = 10
 var good: i32;
 )~~"},
-        {   "invalid globals 11", R"~~(module main;
+        {               "invalid globals 11", R"~~(module main;
 var global =
 var good: i32;
 )~~"},
-        {   "invalid globals 12", R"~~(module main;
+        {               "invalid globals 12", R"~~(module main;
 var global = 10
 var good: i32;
 )~~"},
-        {            "globals 2", R"~~(module main;
+        {                        "globals 2", R"~~(module main;
 var global: usize = 10;
 var good: i32;
 )~~"},
-        {            "globals 3", R"~~(module main;
+        {                        "globals 3", R"~~(module main;
 var global = 10;
 var good: i32;
 )~~"},
 
-        {               "defs 1", R"~~(module main;
+        {                           "defs 1", R"~~(module main;
 def TEN = 10;
 var global = TEN;
 )~~"},
-        {       "invalid defs 1", R"~~(module main;
+        {                   "invalid defs 1", R"~~(module main;
 def
 var global = 10;
 )~~"},
-        {       "invalid defs 2", R"~~(module main;
+        {                   "invalid defs 2", R"~~(module main;
 def TEN
 var global = 10;
 )~~"},
-        {       "invalid defs 3", R"~~(module main;
+        {                   "invalid defs 3", R"~~(module main;
 def TEN =
 var global = 10;
 )~~"},
-        {       "invalid defs 4", R"~~(module main;
+        {                   "invalid defs 4", R"~~(module main;
 def TEN = 10
 var global = TEN;
+)~~"},
+
+        {         "global with attributes 1", R"~~(module main;
+@magic(123, priority=10)
+var thing = 10;
+)~~"},
+        {         "global with attributes 2", R"~~(module main;
+@test(thing=12,)
+var global;
+)~~"},
+        {         "global with attributes 3", R"~~(module main;
+@test_1(12,)
+@test_2
+var global;
+)~~"},
+
+        { "invalid global with attributes 1", R"~~(module main;
+@
+var thing = 10;
+)~~"},
+        { "invalid global with attributes 2", R"~~(module main;
+@
+)~~"},
+        { "invalid global with attributes 3", R"~~(module main;
+@
+var
+)~~"},
+        { "invalid global with attributes 4", R"~~(module main;
+@test(
+var global;
+)~~"},
+        { "invalid global with attributes 5", R"~~(module main;
+@test(
+var global;
+)~~"},
+        { "invalid global with attributes 6", R"~~(module main;
+@test(thing
+var global;
+)~~"},
+        { "invalid global with attributes 7", R"~~(module main;
+@test(thing=
+var global;
+)~~"},
+        { "invalid global with attributes 8", R"~~(module main;
+@test(thing=12
+var global;
+)~~"},
+        { "invalid global with attributes 9", R"~~(module main;
+@test(thing=12,
+var global;
+)~~"},
+        {"invalid global with attributes 10", R"~~(module main;
+@test(thing=)
+var global;
+)~~"},
+        {"invalid global with attributes 11", R"~~(module main;
+@test(thing=,)
+var global;
+)~~"},
+        {"invalid global with attributes 12", R"~~(module main;
+@test_1(
+@test_2
+var global;
+)~~"},
+        {"invalid global with attributes 13", R"~~(module main;
+@test_1(
+@test_2
+var global;
+)~~"},
+        {"invalid global with attributes 14", R"~~(module main;
+@test_1(12
+@test_2
+var global;
+)~~"},
+        {"invalid global with attributes 15", R"~~(module main;
+@test_1(12,
+@test_2
+var global;
+)~~"},
+        {"invalid global with attributes 16", R"~~(module main;
+@test_1(=12,))
+@test_2
+var global;
 )~~"},
     };
 
